7d9824e46002b004a07e3267ed9f0d833e32ad2c
UCHIDA Masaru <usadamasa@gmail.com>
2015-11-11 20:46:40 +0900

    Add README and sh script unnessesary

--- rpm/install_pmccabe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-yum install -y wget
-#wget https://github.com/usadamasa/pmccabe-plugin/releases/download/v1.0/pmccabe-2.6-2.arm64.rpm
-wget https://github.com/usadamasa/pmccabe-plugin/blob/add_rpm/rpm/pmccabe-2.6-2.arm64.rpm
-yum localinstall -y pmccabe-2.6-2.arm64.rpm
-
